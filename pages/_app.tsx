fa24a1167c0592bc395098a3693b71daa4886e8b
Gabriel Ingemarsson <gabriel@ingemarsson.nu>
2021-07-01 20:03:44 +0000

    Merged in bugfix/icon-style-loading (pull request #24)
    Bugfix: Fix icons beeing to large during initial page loading
    
    * Fix icons beeing to large during page load
    
    * Merged dev into bugfix/icon-style-loading
    
    * Merged dev into bugfix/icon-style-loading
    
    * Merged dev into bugfix/icon-style-loading
    
    Approved-by: Markus Wesslén

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 
+// We include the font awesome css here to avoid flashing big icons when the page loads.
+//
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return <Component {...pageProps} />;
 }
