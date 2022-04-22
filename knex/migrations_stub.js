2b766f79a89d2e3804b0af6655ba37783da56d07
Markus Wesslén <markus.wesslen@gmail.com>
2022-04-22 09:34:07 +0000

    Merged in feature/knex-migrations-stub (pull request #104)
    Feature: Add knex migrations stub
    
    Approved-by: Gabriel Ingemarsson

---
+++ knex/migrations_stub.js
@@ -0,0 +1,11 @@
+export function up(knex) {
+    // return knex.schema.alterTable('TableName', (table) => {
+    //     table.int('columnName');
+    // });
+}
+
+export function down(knex) {
+    // return knex.schema.alterTable('TableName', (table) => {
+    //     table.dropColumn('columnName');
+    // });
+}
