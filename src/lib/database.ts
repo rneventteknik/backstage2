cbd6a503704e137aafe5054c5b5db3c517f40b4f
Gabriel Ingemarsson <gabriel@ingemarsson.nu>
2022-07-06 19:52:45 +0000

    Merged in bugfix/to-many-clients-postgres (pull request #144)
    Bugfix: Update database-init state to use env variable
    
    * Update database-init state to use env variable
    
    
    Approved-by: Markus Wesslén

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -2,19 +2,18 @@ import Knex from 'knex';
 import { Model } from 'objection';
 import knexConfiguration from './knexConfiguration.js';
 
-let databaseIsInitialized = false;
-
 // Next.js does not support running code at startup without a custom server, so we
 // initialize the database connection using this method, which should be run before
-// any database operations.
+// any database operations. Next.js disregards the state of this file on reload, so
+// we have to store the DATABASE_IS_INITIALIZED value as a environment variable.
 export const ensureDatabaseIsInitialized: () => void = () => {
-    if (databaseIsInitialized) {
+    if (process.env.DATABASE_IS_INITIALIZED) {
         return;
     }
 
     const knex = Knex<Record<string, unknown>[], Record<string, unknown>[]>(knexConfiguration.development); // TODO: Choose environment intelligently
     Model.knex(knex);
-    databaseIsInitialized = true;
+    process.env.DATABASE_IS_INITIALIZED = 'yes';
 };
 
 export const getDatabaseType: () => string = () => knexConfiguration.development.client;
