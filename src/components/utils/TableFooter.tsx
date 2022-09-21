ae12a1b0cab66d418debeb2c539ab7e996afd5e1
Gabriel Ingemarsson <gabriel@ingemarsson.nu>
2022-09-21 19:31:43 +0000

    Merged in bugfix/table-footer-styling (pull request #165)
    Bugfix: Improve responsive design of table footer controls
    
    * Improve responsive design of table footer controls
    
    Change to flexbox instead of col to improve responsiveness
    
    
    Approved-by: Markus Wesslén

--- src/components/utils/TableFooter.tsx
+++ src/components/utils/TableFooter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Form, Row } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 type Props = {
     viewCount: number;
@@ -16,37 +16,43 @@ const TableFooterWithViewCount: React.FC<Props> = ({
 }: Props) => (
     <>
         <hr />
-        <Row className="my-3 px-2">
-            <Col>
+        <div className="d-flex">
+            <div>
                 <p className="text-muted">
                     Visar {Math.min(viewCount, totalCount)} {entityTypeDisplayName} av {totalCount}.
                 </p>
-            </Col>
-            <Col md="auto">
-                <Form inline>
+            </div>
+            <div className="flex-grow-1 ml-2">
+                <Form>
                     <Form.Group controlId="viewcount">
-                        <Form.Label>Antal {entityTypeDisplayName} att visa</Form.Label>
-                        <Form.Control
-                            as="select"
-                            name="viewcount"
-                            className="ml-2"
-                            size="sm"
-                            defaultValue={viewCount}
-                            onChange={(e) => setViewCount(Number(e.target.value))}
-                        >
-                            <option value="25">25</option>
-                            <option value="50">50</option>
-                            <option value="100">100</option>
-                            <option value="500">500</option>
-                            <option value="1000">1000</option>
-                            <option value={totalCount}>
-                                Alla {totalCount} {entityTypeDisplayName}
-                            </option>
-                        </Form.Control>
+                        <div className="d-flex justify-content-end">
+                            <div>
+                                <Form.Label className="text-right">Antal {entityTypeDisplayName} att visa</Form.Label>
+                            </div>
+                            <div>
+                                <Form.Control
+                                    as="select"
+                                    name="viewcount"
+                                    className="ml-2"
+                                    size="sm"
+                                    defaultValue={viewCount}
+                                    onChange={(e) => setViewCount(Number(e.target.value))}
+                                >
+                                    <option value="25">25</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                    <option value="500">500</option>
+                                    <option value="1000">1000</option>
+                                    <option value={totalCount}>
+                                        Alla {totalCount} {entityTypeDisplayName}
+                                    </option>
+                                </Form.Control>
+                            </div>
+                        </div>
                     </Form.Group>
                 </Form>
-            </Col>
-        </Row>
+            </div>
+        </div>
     </>
 );
 
