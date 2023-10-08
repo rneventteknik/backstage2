5b36512ad1e28853e78745fbcd8a9aba2b725afe
Gabriel Ingemarsson <gabriel@ingemarsson.nu>
2023-10-08 09:33:19 +0000

    Merged in bugfix/improve-logged-out-modal (pull request #368)
    Bugfix: Make the logged out modal a bit smarter
    
    
    Approved-by: Markus Wesslén

--- src/components/layout/LoggedOutModal.tsx
+++ src/components/layout/LoggedOutModal.tsx
@@ -3,6 +3,8 @@ import { Button, Modal } from 'react-bootstrap';
 import { CurrentUserInfo } from '../../models/misc/CurrentUserInfo';
 import { getGlobalSetting } from '../../lib/utils';
 import { KeyValue } from '../../models/interfaces/KeyValue';
+import Router from 'next/router';
+import Link from 'next/link';
 
 type Props = {
     currentUser: CurrentUserInfo;
@@ -21,30 +23,60 @@ const LoggedOutModal: React.FC<Props> = ({ currentUser, globalSettings }: Props)
         };
     }, []);
 
+    const logOut = async () => {
+        const res = await fetch('/api/users/logout');
+        if (res.status === 200) {
+            Router.push(loginPageUrl);
+        }
+    };
+
     // 15 min
     const timeBeforeLogOutToShowModal = 15 * 60 * 1000;
 
     const maxSessionLength = parseInt(getGlobalSetting('config.maxSessionLength', globalSettings, '0'));
     const logOutDatetime = (currentUser.loginDate ?? 0) + maxSessionLength;
     const isSoonLoggedOut = maxSessionLength > 0 && logOutDatetime - timeBeforeLogOutToShowModal < date;
+    const isLoggedOut = maxSessionLength > 0 && logOutDatetime < date;
 
     const minutesToLogOut = Math.round(((currentUser.loginDate ?? 0) + maxSessionLength - date) / 1000 / 60);
+    const loginPageUrl = '/login?redirectUrl=' + Router.asPath;
 
-    return !hideModal && isSoonLoggedOut ? (
+    return (!hideModal && isSoonLoggedOut) || isLoggedOut ? (
         <Modal show={true}>
             <Modal.Header>
-                <Modal.Title>Snart utloggad</Modal.Title>
+                <Modal.Title>{isLoggedOut ? 'Utloggad' : 'Snart utloggad'}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <p>
-                    Din session går snart ut, vänligen spara ditt arbete och logga in igen. Du loggas ut om{' '}
-                    {minutesToLogOut} minuter.
+                    {isLoggedOut ? (
+                        <p>Du är nu utloggad, vänligen logga in igen.</p>
+                    ) : (
+                        <p>
+                            Din session går snart ut, vänligen spara ditt arbete och logga in igen. Du loggas ut om{' '}
+                            {minutesToLogOut} minuter.
+                        </p>
+                    )}
                 </p>
+                <p></p>
             </Modal.Body>
+
             <Modal.Footer>
-                <Button variant="primary" onClick={() => setHideModal(true)}>
-                    Jag förstår
-                </Button>
+                {isLoggedOut ? (
+                    <Link href={loginPageUrl} passHref>
+                        <Button variant="primary" as="span">
+                            Logga in igen
+                        </Button>
+                    </Link>
+                ) : (
+                    <>
+                        <Button variant="secondary" onClick={logOut}>
+                            Logga ut
+                        </Button>
+                        <Button variant="primary" onClick={() => setHideModal(true)}>
+                            Jag förstår
+                        </Button>
+                    </>
+                )}
             </Modal.Footer>
         </Modal>
     ) : null;
