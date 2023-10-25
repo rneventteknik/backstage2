c73581022ac3fe30909f9b0a77b35828f6e8ee50
Gabriel Ingemarsson <gabriel@ingemarsson.nu>
2023-10-25 22:57:13 +0200

    Send slack DM to owner of a booking on update
    DM is send when:
    * Rental status is changed
    * Booking status is changed

--- src/lib/slack.ts
+++ src/lib/slack.ts
@@ -1,12 +1,9 @@
 import { WebClient } from '@slack/web-api';
+import { fetchBookingWithEquipmentLists } from './db-access/booking';
+import { CurrentUserInfo } from '../models/misc/CurrentUserInfo';
 
-export const sendSlackMessage = async (message: string) => {
+export const sendSlackMessage = async (message: string, channelId: string) => {
     const client = new WebClient(process.env.SLACK_BOT_TOKEN);
-    const channelId = process.env.SLACK_CHANNEL_ID;
-
-    if (!channelId) {
-        throw new Error('Invalid slack channel id');
-    }
 
     try {
         await client.chat.postMessage({
@@ -18,9 +15,50 @@ export const sendSlackMessage = async (message: string) => {
     }
 };
 
-export const sendSlackMessageForBooking = async (message: string, bookingId: number, bookingName: string) => {
+export const sendSlackMessageForBooking = async (
+    message: string,
+    bookingId: number,
+    bookingName: string,
+    channelId: string | null = null,
+    heading = '',
+) => {
+    const prefix = heading.length > 0 ? `*${heading}*\n` : '';
     const bookingUrl = process.env.APPLICATION_BASE_URL + '/bookings/' + bookingId;
-    const formattedMessage = `<${bookingUrl}|${bookingName}> - ${message}`;
+    const formattedMessage = `${prefix}<${bookingUrl}|${bookingName}> - ${message}`;
 
-    sendSlackMessage(formattedMessage);
+    const slackChannelId = channelId ?? process.env.SLACK_CHANNEL_ID;
+
+    if (!slackChannelId) {
+        throw new Error('Invalid slack channel id');
+    }
+
+    sendSlackMessage(formattedMessage, slackChannelId);
+};
+
+// Note: The last parameter here is not the user to send to, is the user NOT to send to (since we do not want to send DMs to the user triggering the action, even if they own the booking).
+export const sendSlackDMForBooking = async (
+    message: string,
+    bookingId: number,
+    bookingName: string,
+    currentUser: CurrentUserInfo,
+) => {
+    const booking = await fetchBookingWithEquipmentLists(bookingId);
+    const ownerUserSlackId = booking.ownerUser.slackId;
+
+    if (!ownerUserSlackId) {
+        return;
+    }
+
+    // Only send any message if the current user isnt the owner
+    if (booking.ownerUser.id === currentUser.userId) {
+        return;
+    }
+
+    sendSlackMessageForBooking(
+        message,
+        bookingId,
+        bookingName,
+        ownerUserSlackId,
+        'En bokning du är ansvarig för har uppdaterats',
+    );
 };
