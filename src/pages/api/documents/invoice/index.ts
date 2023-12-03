2feae8e538e9bba1583e48dfb2bec601bb0e2251
Johan Sjöberg <johan.sjoberg@kvicksund.org>
2023-12-03 23:34:16 +0100

    Import equipment

--- src/pages/api/documents/invoice/index.ts
+++ src/pages/api/documents/invoice/index.ts
@@ -56,7 +56,7 @@ const getEncodedInvoices = async (id: number) => {
 
         const streamToBuffer = (stream: NodeJS.ReadableStream): Promise<Buffer> => {
             return new Promise<Buffer>((resolve, reject) => {
-                const _buf = Array<any>();
+                const _buf = Array<Uint8Array>();
                 stream.on('data', (chunk) => _buf.push(chunk));
                 stream.on('end', () => resolve(Buffer.concat(_buf)));
                 stream.on('error', (err) => reject(`error converting stream - ${err}`));
