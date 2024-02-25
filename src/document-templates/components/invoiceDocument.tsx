ab8687b6824cc8860c6de05376e41aecb4a46198
Markus Wesslén <markus.wesslen@gmail.com>
2024-02-25 22:21:59 +0100

    Clarify that invoice data prices are excl VAT

--- src/document-templates/components/invoiceDocument.tsx
+++ src/document-templates/components/invoiceDocument.tsx
@@ -194,13 +194,13 @@ export const InvoiceDocument: React.FC<InvoiceDocumentProps> = ({
                                 <Text style={styles.italic}>{t('common.equipment-list.table-header.count')}</Text>
                             </TableCellFixedWidth>
                             <TableCellFixedWidth width={90} textAlign="right">
-                                <Text style={styles.italic}>{t('common.equipment-list.table-header.price')}</Text>
+                                <Text style={styles.italic}>{t('common.equipment-list.table-header.price-ex-vat')}</Text>
                             </TableCellFixedWidth>
                             <TableCellFixedWidth width={90} textAlign="right">
                                 <Text style={styles.italic}>{t('invoice.account')}</Text>
                             </TableCellFixedWidth>
                             <TableCellFixedWidth width={90} textAlign="right">
-                                <Text style={styles.italic}>{t('common.equipment-list.table-header.total-price')}</Text>
+                                <Text style={styles.italic}>{t('common.equipment-list.table-header.total-price-ex-vat')}</Text>
                             </TableCellFixedWidth>
                         </TableRowWithNoBorder>
                         {invoiceData.invoiceRows.map((invoiceRow, index) => (
