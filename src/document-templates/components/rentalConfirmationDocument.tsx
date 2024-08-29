aaa1bf7975de9ea57f83e801078adb55cc91e15f
Gabriel Ingemarsson <gabriel@ingemarsson.nu>
2024-08-29 17:38:23 +0200

    Feature: Add QR code for evaluation to rental agreement pdf (#57)

--- src/document-templates/components/rentalConfirmationDocument.tsx
+++ src/document-templates/components/rentalConfirmationDocument.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Booking } from '../../models/interfaces';
-import { Page, View, Text, Document, Link } from '@react-pdf/renderer';
+import { Page, View, Text, Document, Image, Link } from '@react-pdf/renderer';
 import { commonStyles } from '../utils';
 import { BookingInfo } from './shared/bookingInfo';
 import { PageCount } from './shared/pageCount';
@@ -16,6 +16,7 @@ import { Col, Row } from './shared/utils';
 import { Signature } from './shared/signature';
 import { getTotalTimeEstimatesPrice } from '../../lib/pricingUtils';
 import { KeyValue } from '../../models/interfaces/KeyValue';
+import { getGlobalSetting } from '../../lib/utils';
 
 type Props = {
     booking: Booking;
@@ -24,6 +25,12 @@ type Props = {
 
 const styles = {
     ...commonStyles,
+    qrCodeImage: {
+        height: '80pt',
+        width: '80pt',
+        marginTop: 4,
+        marginBottom: 20,
+    },
 };
 
 export const RentalConfirmationDocument: React.FC<Props> = ({ booking, globalSettings }: Props) => {
@@ -31,6 +38,12 @@ export const RentalConfirmationDocument: React.FC<Props> = ({ booking, globalSet
 
     const showPrices = booking.fixedPrice === null;
     const showPersonnelCosts = getTotalTimeEstimatesPrice(booking.timeEstimates).value !== 0 && showPrices;
+
+    const qrCodeImageSrc = getGlobalSetting(
+        'content.image.documentRentalAgreementQrCodeImage',
+        globalSettings,
+        '',
+    ).trim();
 
     return (
         <Document title={t('rental-agreement.title')}>
@@ -48,12 +61,24 @@ export const RentalConfirmationDocument: React.FC<Props> = ({ booking, globalSet
                         ))}
                         {showPersonnelCosts ? <TimeEstimateListInfo booking={booking} showPrices={showPrices} /> : null}
                     </View>
-                    <TotalPriceSection
-                        booking={booking}
-                        showPersonnelCosts={showPersonnelCosts}
-                        showEquipmentCosts={showPrices}
-                        priceByAgreement={!showPrices}
-                    />
+                    <View>
+                        {qrCodeImageSrc ? (
+                            <View wrap={false} style={{ position: 'absolute', bottom: 0 }}>
+                                <Row>
+                                    <Image style={styles.qrCodeImage} src={qrCodeImageSrc} />
+                                    <Col>
+                                        <Text style={{ width: 100 }}>{t('rental-agreement.qr-code.description')}</Text>
+                                    </Col>
+                                </Row>
+                            </View>
+                        ) : null}
+                        <TotalPriceSection
+                            booking={booking}
+                            showPersonnelCosts={showPersonnelCosts}
+                            showEquipmentCosts={showPrices}
+                            priceByAgreement={!showPrices}
+                        />
+                    </View>
                     <View wrap={false}>
                         <Text style={[styles.bold, styles.marginTopLarge]}>
                             {t('rental-agreement.legal-note.title')}
