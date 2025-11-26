89a5ef78248b4376a674da4c9d44d5281829b9f7
Herman Karlsson <herkarlsson@gmail.com>
2025-11-26 23:05:42 +0100

    Bugfix: change to localstorage instead of date

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,7 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
             api_host: '/ingest',
             ui_host: 'https://eu.posthog.com',
-            defaults: '2025-05-24',
+            persistence: 'localStorage',
             capture_exceptions: true,
             debug: process.env.NODE_ENV === 'development',
         });
